10935028630cde6a7689cbfaed5c728cf886f8e3
Omar <omarshahid160@gmail.com>
2021-12-25 14:20:13 +0500

    Added sokcet IO URL

--- web/src/common/lib/socket.ts
+++ web/src/common/lib/socket.ts
@@ -2,7 +2,7 @@ import socketIOClient from "socket.io-client";
 import { store } from "../redux/store";
 
 export const socket = socketIOClient.io(
-  process.env.PRODUCTION_BACKEND_URL ??
+  process.env.REACT_APP_PROD_API_URL ??
     process.env.REACT_APP_BACKEND_IP_URL ??
     process.env.REACT_APP_BACKEND_URL ??
     "",
